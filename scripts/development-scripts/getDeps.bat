0046d21c836c8df5ae062881ffca9931c0d6f0e7
Richard Maxwell <richard@rsmaxwell.co.uk>
2024-07-06 09:51:03 +0100

    fix recursion bug in responce.put

--- scripts/development-scripts/getDeps.bat
+++ scripts/development-scripts/getDeps.bat
@@ -45,7 +45,7 @@ set directory=archiva
 set repository=releases
 set group=com.rsmaxwell.mqtt.rpc.example
 set artifact=mqtt-rpc-example-response
-set version=0.0.1.27
+set version=0.0.1.29
 set filename=mqtt-rpc-example-response
 
 set baseURL=%scheme%://%host%/%directory%
